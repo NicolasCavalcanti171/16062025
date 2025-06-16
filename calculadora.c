9cb25a951e3feff67535f34b5394055407c5fbc3
Nicolas Cavalcanti da Silva <nicolas.silva418@etec.sp.gov.br>
2025-06-16 17:53:40 -0300

    início

---
+++ calculadora.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <atdlib.h>
+
+void main(){
+	int n1,n2;
+	int resultado;
+	char operação;
+}
+void soma(){
+	return n1+n2;
+}
+void subtracao(){
+	return n1-n2;
+}
+
+void multi(){
+	return n1*n2;
+}
+
+void divisao(){
+	return n1/n2;
+} 
